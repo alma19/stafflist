be2d749c760c3d9faffdb30741817d7f696d6ce7
alma19 <awashington@TC02MR1Z2FD56L.local>
2018-06-14 17:24:31 -0500

    A lot of this project is done. Can search for staff members.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,11 +7,40 @@ import './furniture';
 
 $(document).ready(() => {
 
-const selectBox = $('#select-person').selectize({
+  const selectBoxes = {
+  };
+
+
+  //
+  $('.select-person').each((i, el) => {
+    const role = $(el).closest('tr').attr('data-role');
+    const day = $(el).closest('td').attr('data-day');
+
+    // writing to selectBoxes
+    if (!Object.prototype.hasOwnProperty.call(selectBoxes, role)) {
+      selectBoxes[role] = {};
+    }
+    selectBoxes[role][day] = $(el).selectize({
       valueField: 'email',
       labelField: 'fullName',
       searchField: 'fullName',
       options: [],
+      render: {
+        item: function(item, escape) {
+            return '<div>' +
+                (item.fullName ? '<p class="name">' + escape(item.fullName) + '</p>' : '') +
+                (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+            '</div>';
+        },
+        option: function(item, escape) {
+            var label = item.fullName || item.email;
+            var caption = item.fullName ? item.email : null;
+            return '<div>' +
+                '<p class="label">' + escape(label) + '</p>' +
+                (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+            '</div>';
+        }
+    },
       persist: false,
       // loadThrottle: 600,
       create: false,
@@ -20,7 +49,11 @@ const selectBox = $('#select-person').selectize({
       //     if (!query.length) return callback();
       //
       // }
-  })
+    });
+
+
+
+  });
 
 
   $.ajax({
@@ -30,10 +63,19 @@ const selectBox = $('#select-person').selectize({
       error: function() {
           callback();
       },
+
+      //looking through the key and value pairs to get to the selectize option
       success: function(staffList) {
+        Object.keys(selectBoxes).forEach(function(roleName){
+          var roleValue = selectBoxes[roleName];
+          Object.keys(roleValue).forEach(function(dayName){
+            var dayValue = roleValue[dayName];
+            dayValue[0].selectize.addOption(staffList);
+          })
+      })
           // you can apply any modification to data before passing it to selectize
           // callback(res);
-          selectBox[0].selectize.addOption(staffList);
+
           // res is json response from server
           // it contains array of objects. Each object has two properties. In this case 'id' and 'Name'
           // if array is inside some other property of res like 'response' or something. than use this
@@ -41,3 +83,6 @@ const selectBox = $('#select-person').selectize({
       }
   });
 });
+
+// replacing the options :
+// find from selecboxes
